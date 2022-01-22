ac7f988b193ee5c0ff56fb91d67752d37f3962d7
Roger Fan <rogerjoeyfan@gmail.com>
2022-01-21 21:33:42 -0800

    Add resources

--- pages/index.js
+++ pages/index.js
@@ -31,8 +31,10 @@ export default function Home() {
 							</a> (Required <span className={styles.em}>Before</span> Workshop!)
 						</li>
 						<li className={styles.purple}>Basics of HTML / CSS / JS</li>
-						<li>Github Repository</li>
-						<li>Figma</li>
+						<li><a href="https://github.com/ImNotRog/GunnHacks-8.0-React-Demo" target="_blank" rel="noopener noreferrer">
+							Github Repository
+						</a></li>
+						<li><a href="https://www.figma.com/file/2imkRBoOLdso3LcK5LxU7U/GunnHacks-8.0-React-Workshop?node-id=0%3A1" target="_blank" rel="noopener noreferrer">Figma</a></li>
 					</ul>
 
 					<p>
@@ -40,18 +42,47 @@ export default function Home() {
 						please get set up! Refer to the first link above for instructions.
 					</p>
 					<p>
-						If you are new to web development, we highly suggest you look over our short blurb on 
+						Additionally, if you are new to web development, we highly suggest you look over our short blurb on 
 						HTML, CSS, and JS before attending the workshop.
 					</p>
 					<p>
-						Finally, feel free to take a look around this website and its code! 
+						Feel free to take a look around this website and its code! 
 						Excepting this landing page, we&apos;ll be creating the following pages, 
 						which are also located in the nav bar:
 					</p>
 
 					<ul>
-						<li><Link href="/"><a>Introductory Counter</a></Link></li>
+						<li><Link href="/counter"><a>Introductory Counter</a></Link></li>
 						<li><Link href="/todo"><a>To-Do List</a></Link></li>
+					</ul>
+
+					<p>
+						We realize that we would never cover the full scope of React within an hour. 
+						Provided below are excellent resources to further your journey in web development!
+					</p>
+
+					<ul>
+						<li><a href="https://vercel.com/docs">Next JS Documentation</a></li>
+						<li>Backend Resources 
+							<ul>
+								<li><a href="https://vercel.com/docs/concepts/solutions/databases">Options for Databases with Next JS</a></li>
+								<li><a href="https://vercel.com/docs/concepts/solutions/realtime">Options for Realtime Apps, like Chat or Games</a>
+									<ul>
+										<li><a href="https://vercel.com/integrations/supabase">Supabase is an excellent option for the above two points</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+
+						<li>CSS Resources
+							<ul>
+								<li><a href="https://css-tricks.com/guides/">CSS Tricks - a fantastic website for CSS solutions</a></li>
+								<li><a href="https://cssreference.io/">CSS Reference</a></li>
+							</ul>
+						</li>
+
+
+						<li>Google. When in doubt, Google is your friend.</li>
 					</ul>
 				</div>
 			</div>
